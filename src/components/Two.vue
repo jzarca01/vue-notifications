89d2a32c357445658e613e6fd2311a6374567988
Sergey Panfilov <se-panfilov@ya.ru>
2017-04-11 11:48:45 +0800

    remove inline, watch and computed

--- src/components/Two.vue
+++ src/components/Two.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h4>This is TWO</h4>
-    <button type="button" @click="warnMsg()">Show from component Two</button>
+    <button type="button" @click="warnMsg()">Show 'warn' from component Two</button>
+    <button type="button" @click="successMsg()">Show 'success' from component Two</button>
   </div>
 </template>
 
@@ -15,6 +16,11 @@
       warnMsg: {
         type: 'warn',
         message: 'Component TWO warning'
+      },
+      successMsg: {
+        type: 'success',
+        title: 'Success!',
+        message: 'Component TWO success'
       }
     }
   }
