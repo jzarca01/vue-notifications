4a2b0fa2bfc3987fa24dfb24b0fee5947175e266
Sergei <se-panfilov@ya.ru>
2017-11-28 15:19:43 +0800

    fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default [
@@ -12,20 +13,16 @@ export default [
     },
     name: 'vue-notifications',
     plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs() // so Rollup can convert `ms` to an ES module
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**'
+      })
     ]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/main.js',
-    external: ['ms'],
+    // external: ['ms'],
     output: [
       {file: pkg.main, format: 'cjs'},
       {file: pkg.module, format: 'es'}
