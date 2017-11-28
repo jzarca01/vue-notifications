c72cb5bbeed28cb0241b9a8c6d2252266ba35931
Sergei <se-panfilov@ya.ru>
2017-11-28 23:01:19 +0800

    fixes

--- dist/vue-notifications.js
+++ dist/vue-notifications.js
@@ -1,69 +1,90 @@
-'use strict';
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['vue-notifications'] = factory());
+}(this, (function () { 'use strict';
 
-const PLUGIN_NAME = 'VueNotifications';
-const PACKAGE_NAME = 'vue-notifications';
-const PROPERTY_NAME = 'notifications';
+var defineProperty = function (obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
 
-const TYPES = {
+  return obj;
+};
+
+var PLUGIN_NAME = 'VueNotifications';
+var PACKAGE_NAME = 'vue-notifications';
+var PROPERTY_NAME = 'notifications';
+
+var TYPES = {
   error: 'error',
   warn: 'warn',
   info: 'info',
   success: 'success'
 };
 
-const EVANGELION = 1;
+var EVANGELION = 1;
 
-const MESSAGES = {
-  alreadyInstalled: `${PLUGIN_NAME}: plugin already installed`,
-  methodNameConflict: `${PLUGIN_NAME}: names conflict - `
+var MESSAGES = {
+  alreadyInstalled: PLUGIN_NAME + ': plugin already installed',
+  methodNameConflict: PLUGIN_NAME + ': names conflict - '
 };
 
-function getVersion (Vue) {
-  const version = Vue.version.match(/(\d+)/g);
-  return +version[0]
+function getVersion(Vue) {
+  var version = Vue.version.match(/(\d+)/g);
+  return +version[0];
 }
 
-function showDefaultMessage ({type, message, title}) {
-  let msg = `Title: ${title}, Message: ${message}, Type: ${type}`;
-  if (type === TYPES.error) console.error(msg);
-  else if (type === TYPES.warn) console.warn(msg);
-  else console.log(msg);
+function showDefaultMessage(_ref) {
+  var type = _ref.type,
+      message = _ref.message,
+      title = _ref.title;
+
+  var msg = 'Title: ' + title + ', Message: ' + message + ', Type: ' + type;
+  if (type === TYPES.error) console.error(msg);else if (type === TYPES.warn) console.warn(msg);else console.log(msg);
 }
 
-function getValues (vueApp, config) {
-  const result = {};
+function getValues(vueApp, config) {
+  var result = {};
 
-  Object.keys(config).forEach(field => {
+  Object.keys(config).forEach(function (field) {
     if (field === 'cb') {
       result[field] = config[field].bind(vueApp);
     } else {
-      result[field] = (typeof config[field] === 'function') ? config[field].call(vueApp) : config[field];
+      result[field] = typeof config[field] === 'function' ? config[field].call(vueApp) : config[field];
     }
   });
 
-  return result
+  return result;
 }
 
-function showMessage (config, vueApp) {
-  const valuesObj = getValues(vueApp, config);
-  const isMethodOverridden = VueNotifications.pluginOptions[valuesObj.type];
-  const method = isMethodOverridden ? VueNotifications.pluginOptions[valuesObj.type] : showDefaultMessage;
+function showMessage(config, vueApp) {
+  var valuesObj = getValues(vueApp, config);
+  var isMethodOverridden = VueNotifications.pluginOptions[valuesObj.type];
+  var method = isMethodOverridden ? VueNotifications.pluginOptions[valuesObj.type] : showDefaultMessage;
   method(valuesObj, vueApp);
 
-  if (config.cb) return config.cb()
+  if (config.cb) return config.cb();
 }
 
-function addMethods (targetObj, typesObj) {
-  Object.keys(typesObj).forEach(v => {
+function addMethods(targetObj, typesObj) {
+  Object.keys(typesObj).forEach(function (v) {
     targetObj[typesObj[v]] = function (config) {
       config.type = typesObj[v];
       // TODO (S.Panfilov) fix 'vueApp' in param
-      return showMessage(config)
+      return showMessage(config);
     };
   });
 }
 
-function setMethod (vueApp, name, options, pluginOptions) {
+function setMethod(vueApp, name, options, pluginOptions) {
   if (!options.methods) options.methods = {};
 
   // ///////////////////////////////////////////////////////////////////////
@@ -84,54 +105,52 @@ function setMethod (vueApp, name, options, pluginOptions) {
   }
 }
 
-function makeMethod (vueApp, configName, options, pluginOptions) {
+function makeMethod(vueApp, configName, options, pluginOptions) {
   return function (config) {
-    const newConfig = Object.assign({},
-      VueNotifications.config,
-      options[VueNotifications.propertyName][configName],
-      config);
+    var newConfig = Object.assign({}, VueNotifications.config, options[VueNotifications.propertyName][configName], config);
 
-    return showMessage(newConfig, pluginOptions, vueApp)
-  }
+    return showMessage(newConfig, pluginOptions, vueApp);
+  };
 }
 
-function initVueNotificationPlugin (vueApp, notifications, pluginOptions) {
-  if (!notifications) return
-  Object.keys(notifications).forEach(name => setMethod(vueApp, name, vueApp.$options, pluginOptions));
-  vueApp.$emit(`${PACKAGE_NAME}-initiated`);
+function initVueNotificationPlugin(vueApp, notifications, pluginOptions) {
+  if (!notifications) return;
+  Object.keys(notifications).forEach(function (name) {
+    return setMethod(vueApp, name, vueApp.$options, pluginOptions);
+  });
+  vueApp.$emit(PACKAGE_NAME + '-initiated');
 }
 
-function unlinkVueNotificationPlugin (vueApp, notifications) {
-  if (!notifications) return
-  const attachedMethods = vueApp.$options.methods;
-  Object.keys(notifications).forEach(name => {
+function unlinkVueNotificationPlugin(vueApp, notifications) {
+  if (!notifications) return;
+  var attachedMethods = vueApp.$options.methods;
+  Object.keys(notifications).forEach(function (name) {
     if (attachedMethods[name]) {
       attachedMethods[name] = undefined;
       delete attachedMethods[name];
     }
   });
 
-  vueApp.$emit(`${PACKAGE_NAME}-unlinked`);
+  vueApp.$emit(PACKAGE_NAME + '-unlinked');
 }
 
-function makeMixin (Vue, pluginOptions) {
-  const init = getVersion(Vue) === EVANGELION ? 'init' : 'beforeCreate';
+function makeMixin(Vue, pluginOptions) {
+  var _ref2;
 
-  return {
-    [init]: function () {
-      //this === vueApp
-      const notificationsField = this.$options[VueNotifications.propertyName];
-      initVueNotificationPlugin(this, notificationsField, pluginOptions);
-    },
-    'beforeDestroy': function () {
-      //this === vueApp
-      const notificationsField = this.$options[VueNotifications.propertyName];
-      unlinkVueNotificationPlugin(this, notificationsField);
-    }
-  }
+  var init = getVersion(Vue) === EVANGELION ? 'init' : 'beforeCreate';
+
+  return _ref2 = {}, defineProperty(_ref2, init, function () {
+    //this === vueApp
+    var notificationsField = this.$options[VueNotifications.propertyName];
+    initVueNotificationPlugin(this, notificationsField, pluginOptions);
+  }), defineProperty(_ref2, 'beforeDestroy', function beforeDestroy() {
+    //this === vueApp
+    var notificationsField = this.$options[VueNotifications.propertyName];
+    unlinkVueNotificationPlugin(this, notificationsField);
+  }), _ref2;
 }
 
-const VueNotifications = {
+var VueNotifications = {
   types: TYPES,
   propertyName: PROPERTY_NAME,
   config: {
@@ -146,9 +165,11 @@ const VueNotifications = {
    * @param  {Object} pluginOptions
    * @this VueNotifications
    */
-  install (Vue, pluginOptions = {}) {
-    if (this.installed) throw console.error(MESSAGES.alreadyInstalled)
-    const mixin = makeMixin(Vue, pluginOptions);
+  install: function install(Vue) {
+    var pluginOptions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+
+    if (this.installed) throw console.error(MESSAGES.alreadyInstalled);
+    var mixin = makeMixin(Vue, pluginOptions);
     Vue.mixin(mixin);
 
     this.setPluginOptions(pluginOptions);
@@ -156,12 +177,15 @@ const VueNotifications = {
 
     this.installed = true;
   },
-  setPluginOptions (options = {}) {
+  setPluginOptions: function setPluginOptions() {
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
     this.pluginOptions = options;
   }
 
   //TODO (S.Panfilov) add ability to access this.notifications.someError.message
   //TODO (S.Panfilov) add "noCall:true" property
+
 };
 
 if (typeof window !== 'undefined' && window.Vue) {
@@ -187,7 +211,9 @@ VueNotifications._private.PROPERTY_NAME = PROPERTY_NAME;
 VueNotifications._private.EVANGELION = EVANGELION;
 VueNotifications._private.MESSAGES = MESSAGES;
 
-
 /*END.TESTS_ONLY*/
 
-module.exports = VueNotifications;
+return VueNotifications;
+
+})));
+//# sourceMappingURL=vue-notifications.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,37 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+const externalDeps = Object.keys(Object.assign({}, pkg.dependencies, pkg.peerDependencies))
+const nodeDeps = ['path']
+const external = externalDeps.concat(nodeDeps)
+
+const pluginName = 'vue-notifications'
 
 export default [
-  // browser-friendly UMD build
   {
     input: 'src/main.js',
     output: {
       file: pkg.browser,
       format: 'umd'
     },
-    name: 'vue-notifications',
+    name: pluginName
+  },
+  {
+    input: 'src/main.js',
+    // external: ['ms'],
+    external: external,
+    output: [
+      {file: pkg.main, format: 'umd', name: pluginName}
+    ],
+    sourcemap: true,
     plugins: [
       resolve(),
       commonjs(),
       babel({
         exclude: 'node_modules/**'
       })
-    ]
-  },
-  {
-    input: 'src/main.js',
-    // external: ['ms'],
-    output: [
-      {file: pkg.main, format: 'cjs'},
-      {file: pkg.module, format: 'es'}
     ]
   }
 ];
